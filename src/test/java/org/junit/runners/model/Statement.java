64e46794744c86a231bc01fcddc5c97cf3e77ea3
Andreas Hufler <andreas.hufler@aboutbits.it>
2024-09-26 15:14:23 +0200

    edit comments

--- src/test/java/org/junit/runners/model/Statement.java
+++ src/test/java/org/junit/runners/model/Statement.java
@@ -3,7 +3,7 @@ package org.junit.runners.model;
 /**
  * This is required by Testcontainers but actually unused.
  * By defining this here, we can avoid loading Junit-4.
- * See: https://github.com/testcontainers/testcontainers-java/issues/970
+ * See: https://github.com/testcontainers/testcontainers-java/issues/970#issuecomment-625044008
  */
 @SuppressWarnings("unused")
 public class Statement {
