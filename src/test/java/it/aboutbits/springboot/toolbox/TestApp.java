6e9447a99fa17988519371ac1d285e219469232a
Andreas Hufler <andreas.hufler@aboutbits.it>
2025-08-06 14:58:15 +0200

    remove old annotation

--- src/test/java/it/aboutbits/springboot/toolbox/TestApp.java
+++ src/test/java/it/aboutbits/springboot/toolbox/TestApp.java
@@ -1,6 +1,5 @@
 package it.aboutbits.springboot.toolbox;
 
-import it.aboutbits.springboot.toolbox.autoconfiguration.swagger.RegisterCustomTypesWithSwagger;
 import it.aboutbits.springboot.toolbox.autoconfiguration.web.RegisterCustomTypesWithJacksonAndMvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
 @RegisterCustomTypesWithJacksonAndMvc
-@RegisterCustomTypesWithSwagger
 public class TestApp {
 
     public static void main(String[] args) {
