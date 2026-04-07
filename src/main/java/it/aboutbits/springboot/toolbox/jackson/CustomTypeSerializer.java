f83959b6b1f4ce4216522e7dc3952bb3211b5899
voidstar <pitiz29a@gmail.com>
2026-04-07 15:53:00 +0200

    add numeric values to serializer (#61)

--- src/main/java/it/aboutbits/springboot/toolbox/jackson/CustomTypeSerializer.java
+++ src/main/java/it/aboutbits/springboot/toolbox/jackson/CustomTypeSerializer.java
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import it.aboutbits.springboot.toolbox.type.CustomType;
+import it.aboutbits.springboot.toolbox.type.ScaledBigDecimal;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.UUID;
 
 public class CustomTypeSerializer extends JsonSerializer<CustomType<?>> {
@@ -27,6 +30,15 @@ public class CustomTypeSerializer extends JsonSerializer<CustomType<?>> {
             case String stringValue -> jsonGenerator.writeString(stringValue);
             case UUID uuidValue -> jsonGenerator.writeString(uuidValue.toString());
             case Enum<?> enumValue -> jsonGenerator.writeString(enumValue.name());
+            case Byte numericValue -> jsonGenerator.writeNumber(numericValue);
+            case Short numericValue -> jsonGenerator.writeNumber(numericValue);
+            case Integer numericValue -> jsonGenerator.writeNumber(numericValue);
+            case Long numericValue -> jsonGenerator.writeNumber(numericValue);
+            case Float numericValue -> jsonGenerator.writeNumber(numericValue);
+            case Double numericValue -> jsonGenerator.writeNumber(numericValue);
+            case BigInteger numericValue -> jsonGenerator.writeNumber(numericValue);
+            case BigDecimal numericValue -> jsonGenerator.writeNumber(numericValue);
+            case ScaledBigDecimal numericValue -> jsonGenerator.writeNumber(numericValue.doubleValue());
             case null -> jsonGenerator.writeNull();
             default -> jsonGenerator.writeRawValue(String.valueOf(value));
         }
