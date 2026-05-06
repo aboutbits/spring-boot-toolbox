974b5b3aa9f71c0159b183cbc4100c0c9beb8779
Andreas Hufler <andreas.hufler@aboutbits.it>
2026-05-06 08:28:16 +0200

    add missing annotations

--- src/test/java/it/aboutbits/springboot/toolbox/reflection/util/testfixtures/ScanTestAbstractImpl.java
+++ src/test/java/it/aboutbits/springboot/toolbox/reflection/util/testfixtures/ScanTestAbstractImpl.java
@@ -1,4 +1,7 @@
 package it.aboutbits.springboot.toolbox.reflection.util.testfixtures;
 
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
 public abstract class ScanTestAbstractImpl implements ScanTestInterface {
 }
--- src/test/java/it/aboutbits/springboot/toolbox/reflection/util/testfixtures/ScanTestConcreteImpl.java
+++ src/test/java/it/aboutbits/springboot/toolbox/reflection/util/testfixtures/ScanTestConcreteImpl.java
@@ -1,4 +1,7 @@
 package it.aboutbits.springboot.toolbox.reflection.util.testfixtures;
 
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
 public class ScanTestConcreteImpl implements ScanTestInterface {
 }
