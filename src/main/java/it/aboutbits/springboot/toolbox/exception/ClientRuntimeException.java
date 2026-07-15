d0b026095744ea3a8d320cd10fa5c7de953c35cf
Peter Moser <peter.moser@aboutbits.it>
2026-07-15 10:53:12 +0200

    add object args to ClientRuntimeException

--- src/main/java/it/aboutbits/springboot/toolbox/exception/ClientRuntimeException.java
+++ src/main/java/it/aboutbits/springboot/toolbox/exception/ClientRuntimeException.java
@@ -4,27 +4,46 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public class ClientRuntimeException extends RuntimeException {
+
+    private final Object[] args;
+
     public ClientRuntimeException() {
+        this.args = new Object[0];
     }
 
     public ClientRuntimeException(ExceptionMessageDefinition message) {
         super(message.code());
+        this.args = new Object[0];
+    }
+
+    public ClientRuntimeException(ExceptionMessageDefinition message, Object... args) {
+        super(message.code());
+        this.args = args;
     }
 
     public ClientRuntimeException(String message) {
         super(message);
+        this.args = new Object[0];
+    }
+
+    public ClientRuntimeException(String message, Object... args) {
+        super(message);
+        this.args = args;
     }
 
     public ClientRuntimeException(ExceptionMessageDefinition message, Throwable cause) {
         super(message.code(), cause);
+        this.args = new Object[0];
     }
 
     public ClientRuntimeException(String message, Throwable cause) {
         super(message, cause);
+        this.args = new Object[0];
     }
 
     public ClientRuntimeException(Throwable cause) {
         super(cause);
+        this.args = new Object[0];
     }
 
     public ClientRuntimeException(
@@ -34,6 +53,7 @@ public class ClientRuntimeException extends RuntimeException {
             boolean writableStackTrace
     ) {
         super(message.code(), cause, enableSuppression, writableStackTrace);
+        this.args = new Object[0];
     }
 
     public ClientRuntimeException(
@@ -43,5 +63,6 @@ public class ClientRuntimeException extends RuntimeException {
             boolean writableStackTrace
     ) {
         super(message, cause, enableSuppression, writableStackTrace);
+        this.args = new Object[0];
     }
 }
